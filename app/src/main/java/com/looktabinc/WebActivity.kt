57845be411358571d8dee6b34bab827a041f6786
yuchocopie <pointu341@gmail.com>
2022-11-05 00:16:27 +0900

    앱 아이콘 넣기

---
+++ app/src/main/java/com/looktabinc/WebActivity.kt
@@ -0,0 +1,23 @@
+package com.looktabinc
+
+import android.webkit.WebSettings
+import com.looktabinc.databinding.ActivityWebBinding
+import com.wikitude.architect.ArchitectStartupConfiguration
+
+
+class WebActivity : BaseActivity<ActivityWebBinding>(R.layout.activity_web) {
+
+    val url = "file:///android_asset/index2.html"
+    val url2 = "file:///android_asset/www/index.html"
+
+    override fun initViews() {
+        val settings: WebSettings = binding.architectView.settings
+        settings.allowFileAccess = true
+        binding.architectView.settings.javaScriptEnabled = true
+        binding.architectView.loadUrl(url2)
+
+//        binding.architectView.loadUrl("file:///android_asset/index2.html")
+
+
+    }
+}
